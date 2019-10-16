f41d884a41dfa1a26640e410b13352ff9c270b54
Sion <im.wwz@outlook.com>
2019-10-16 13:02:23 +0800

    完善左侧滑界面

--- app/src/main/java/com/sion/zhdaily/views/activities/MainActivity.java
+++ app/src/main/java/com/sion/zhdaily/views/activities/MainActivity.java
@@ -2,19 +2,19 @@ package com.sion.zhdaily.views.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Toolbar;
 
-import com.google.android.material.navigation.NavigationView;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.viewpager.widget.ViewPager;
+
 import com.sion.zhdaily.R;
 import com.sion.zhdaily.presenters.NewsSummariesHelper;
 import com.sion.zhdaily.views.adapters.NewsSummaryListRvAdapter;
 import com.sion.zhdaily.views.adapters.TopNewsSummaryPagerAdapter;
 import com.sion.zhdaily.views.views.NewsSummaryListRecyclerView;
-
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.viewpager.widget.ViewPager;
 
 
 public class MainActivity extends Activity {
@@ -30,10 +30,7 @@ public class MainActivity extends Activity {
 
     DrawerLayout dl = null;
 
-    NavigationView nv = null;
-
     public NewsSummariesHelper helper = new NewsSummariesHelper();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +40,9 @@ public class MainActivity extends Activity {
         dl = findViewById(R.id.dl);
 
         tb = findViewById(R.id.tb);
-
-        nv = findViewById(R.id.rl_navigationView);
+        tb.setNavigationOnClickListener((v) -> {
+            dl.openDrawer(Gravity.LEFT);
+        });
 
         headerView = getLayoutInflater().inflate(R.layout.rv_summary_header_view, null, false);
         vp = headerView.findViewById(R.id.vp);
@@ -71,4 +69,13 @@ public class MainActivity extends Activity {
 
     }
 
+    @Override
+    public void onBackPressed() {
+        //左边DrawerLayout打开的情况下则关闭
+        if (dl.isDrawerOpen(Gravity.LEFT)) {
+            dl.closeDrawer(Gravity.LEFT);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
